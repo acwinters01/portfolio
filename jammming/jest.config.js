3fb4268b31ab9dda1eaf66612665b996f8b6c90a
acwinters01 <acwinters03@gmail.com>
2025-02-24 20:45:53 -0600

    Removed project folders from portfolio repository

--- jammming/jest.config.js
+++
@@ -1,8 +0,0 @@
-console.log("Jest config is being loaded"); 
-module.exports = {
-    testEnvironment: 'jsdom',
-    setupFilesAfterEnv: ['./util.js'],
-    verbose: true,
-    silent: false,
-};
-  
